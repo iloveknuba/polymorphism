9f75adc8d0247e336a245e8f811261ab5efdca31
iloveknuba <xxxcccer982@gmail.com>
2022-12-24 20:07:40 +0200

    Добавьте файлы проекта.

---
+++ polymorphism/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace polymorphism
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(463, 348);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(567, 405);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(382, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter amount of elements";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ polymorphism/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            NasPunkt[] a = new NasPunkt[n * 3];
+            for (int i = 0; i < 3 * n; i = i + 3)
+            {
+                a[i] = new NasPunkt();
+                a[i + 1] = new Village();
+                a[i + 2] = new City();
+            }
+            dataGridView1.Rows.Clear();
+            foreach (NasPunkt el in a)
+                el.Show(dataGridView1);
+
+        }
+        class NasPunkt
+        {
+            public string naz;
+            public double pl;
+            public NasPunkt()
+            {
+                Random ran = new Random();
+                int r = ran.Next(1, 2);
+                switch (r)
+                {
+                    case 1: { naz = "Village";  break; }
+                    case 2: { naz = "City";  break; }
+                }
+
+
+            }
+            public NasPunkt(string n)
+            {
+                naz = n;
+            }
+            public virtual void Show(DataGridView dg)
+            {
+                dg.Rows.Add("Name", naz);
+                dg.Rows.Add("Square", pl.ToString());
+            }
+            public virtual double S()
+            {
+                return 0;
+            }
+
+        }
+
+
+        class Village : NasPunkt
+        {
+            private int AmountOfHouses;
+            private int AmountOfPeopleInHouses;
+            public Village()
+            {
+                Random ran = new Random();
+                AmountOfHouses = ran.Next(10, 120);
+                AmountOfPeopleInHouses = ran.Next(1, 5);
+                pl = ran.Next(5, 10);
+                naz = "Village";
+            }
+            public override double S()
+            {
+                double sch;
+                sch = AmountOfHouses * AmountOfPeopleInHouses / pl;
+                return sch;
+            }
+            public override void Show(DataGridView dg)
+            {
+
+                base.Show(dg);
+                dg.Rows.Add("Amount of houses ", AmountOfHouses);
+                dg.Rows.Add("Amount of people in houses", AmountOfPeopleInHouses);
+                dg.Rows.Add("population density", S());
+            }
+        }
+
+        class City : NasPunkt
+        {
+            private int AmountOfPeople;
+            Random ran = new Random();
+            public City()
+            {
+                AmountOfPeople = ran.Next(100000, 300000);
+                pl = ran.Next(30, 60);
+                naz = "City";
+            }
+            public override double S()
+            {
+                double s;
+                s = AmountOfPeople / pl;
+                return s;
+            }
+            public override void Show(DataGridView dg)
+            {
+
+                base.Show(dg);
+                dg.Rows.Add("Amount of people", AmountOfPeople.ToString());
+                dg.Rows.Add("population density", S());
+            }
+
+        }
+    }
+}
